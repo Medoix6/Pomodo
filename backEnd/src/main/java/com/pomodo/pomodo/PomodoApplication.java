030cf39ff0ad452cad550fa34054e5911b134155
Mohamed Sami <mohamedsami.per@gmail.com>
2025-10-26 19:57:10 +0800

    Initial Setup / WIP1

---
+++ backEnd/src/main/java/com/pomodo/pomodo/PomodoApplication.java
@@ -0,0 +1,13 @@
+package com.pomodo.pomodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/pomodo/pomodo/PomodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomodo.pomodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
